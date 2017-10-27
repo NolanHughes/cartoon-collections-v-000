17b11ebacaf77054c07ea6f4958a40d0f0548910
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 19:13:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
   array.any? do |i|
     i.length > 4
   end
-  # 
+  #
   # array.find do |i|
   #   if i.length > 4
   #     true
@@ -21,7 +21,7 @@ def long_planeteer_calls(array)
   #     false
   #   end
   # end
-  # 
+  #
   # if array.length > 4
   #   true
   # else
