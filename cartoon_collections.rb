f1530567976568baaec8799fcf23b373c5afe4bd
Nolan Hughes <nhughes987@gmail.com>
2017-10-27 19:10:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarf_name_array)
-  dwarf_name_array.each do |name|
+  dwarf_name_array.each_with_index(1) do |name|
     index_plus_1 = dwarf_name_array.index(name) + 1
     puts "#{index_plus_1} #{name}"
   end
